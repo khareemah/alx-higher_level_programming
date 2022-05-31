08ee5fb33cfee09de2c86406d06d39cac387225c
Khareemah <ajimobikareemah@gmail.com>
2022-05-31 05:37:45 -0700

    print last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def print_last_digit(number):
+    last_digit = None
+    if(number < 0):
+        last_digit = (-1 * number) % 10
+    else:
+        last_digit = number % 10
+    return last_digit
