7da04dd2fadabdb503bae45ff001541c9d36610c
Khareemah <ajimobikareemah@gmail.com>
2022-05-31 07:34:07 -0700

    print uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        asc = ord(i)
+        if(asc >= 97 or asc <= 122):
+            asc = asc - 32
+        print("{}".format(chr(asc)), end="")
