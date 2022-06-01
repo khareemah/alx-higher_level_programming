e17dd36c4bcfbac36f0cf5db09720d618576a9eb
Khareemah <ajimobikareemah@gmail.com>
2022-06-01 07:50:12 -0700

    convert to capital letters

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@ def uppercase(str):
     for i in str:
         asc = ord(i)
         if(asc >= 97 and asc <= 122):
-           asc = asc - 32
+            asc = asc - 32
         print("{}".format(chr(asc)), end="")
     print("")
