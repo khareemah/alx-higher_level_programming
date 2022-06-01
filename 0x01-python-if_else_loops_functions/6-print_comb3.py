1a6b20470876cc02089a77084ab60bb57b7b7961
Khareemah <ajimobikareemah@gmail.com>
2022-06-01 07:08:44 -0700

    print all possible combinations of two numbers

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,7 @@ for i in range(0, 10):
     for j in range(0, 10):
         if(j <= i):
             continue
-        print("{}{}".format(i, j), end=" ")
+        elif(i == 8 and j == 9):
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}, ".format(i, j), end=" ")
