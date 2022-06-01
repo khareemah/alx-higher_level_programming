cd94507beb0e2cf4bf1e938716d90d0622a39adb
Khareemah <ajimobikareemah@gmail.com>
2022-06-01 06:43:38 -0700

    account for beginning and en of the node

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -21,7 +21,7 @@ listint_t *insert_node(listint_t **head, int number)
 	if(*head == NULL)
 		*head = newnode;
 	temp = *head;
-	while (temp->next != NULL)
+	while (newnode->n > temp->n && newnode->n > temp->next->n)
 		temp = temp->next;
 	newnode->next = temp->next;
 	temp->next = newnode;
