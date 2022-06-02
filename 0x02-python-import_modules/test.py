0bec04807d81c140cb781c10a1312cb7d4cc0a5a
Khareemah <ajimobikareemah@gmail.com>
2022-06-02 04:02:35 -0700

    printing to stout

---
+++ 0x02-python-import_modules/test.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+#import os
+#os.system("echo '#pythoniscool'")
+#(echo "import sys" ; echo "for r in range(10): print('rob')") | python3
+python3 "exec(\"import sys\nfor r in range(10): print('rob')\")"
