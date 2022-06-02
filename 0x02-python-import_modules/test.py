2914f56aaf3be809c3b21b170ed2829c41502ab1
Khareemah <ajimobikareemah@gmail.com>
2022-06-02 04:32:52 -0700

    prints 'python is cool in the hard way

--- 0x02-python-import_modules/test.py
+++ 0x02-python-import_modules/test.py
@@ -2,4 +2,5 @@
 #import os
 #os.system("echo '#pythoniscool'")
 #(echo "import sys" ; echo "for r in range(10): print('rob')") | python3
-python3 "exec(\"import sys\nfor r in range(10): print('rob')\")"
+#exec('import os\nos.system("echo \'#pytoniscool\'")')
+exec("import os\nos.system(\"echo \'#pytoniscool\'\")")
