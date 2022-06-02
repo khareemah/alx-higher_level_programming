4186e0ee7206c3744b63a37daade693b895112d9
Khareemah <ajimobikareemah@gmail.com>
2022-06-02 07:56:19 -0700

    fix a bug

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -15,7 +15,7 @@ if(__name__ == "__main__"):
         print("{} + {} = {}".format(a, b, add(a, b)))
     elif(operator == '-'):
         print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif(operator == '\`*'):
+    elif(operator == '\*'):
         print("{} * {} = {}".format(a, b, mul(a, b)))
     elif(operator == '/'):
         print("{} / {} = {}".format(a, b, div(a, b)))
