9cb1052665831588dbf62fca2d86384855a3b42b
Khareemah <ajimobikareemah@gmail.com>
2022-06-02 08:14:54 -0700

    add two numbers

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 from add_0 import add
-def add(a, b):
+if __name__ == "__main__":
     a = 1
     b = 2
-    c = a + b
-    print("{a} + {b} = {c}".format(a, b, c))
-if(__name__ = "__main__"):
-    add(a,b)
-
+    print("{} + {} = {}".format(a, b, add(a, b)))
