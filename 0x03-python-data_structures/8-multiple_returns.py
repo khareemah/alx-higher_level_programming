a45fe1a72f3366f3498d1ed11c88fe7df17022b9
Khareemah <ajimobikareemah@gmail.com>
2022-06-06 04:31:05 -0700

    multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    my_list = [None]
-    my_length = len(sentence)
-    if my_length != 0:
-        my_list[0] = my_length
-    my_tuple = tuple(my_list)
-    return my_length, sentence[0]
+    my_len = len(sentence)
+    if my_len == 0:
+        return (my_len, None)
+    return (my_len, sentence[0])
