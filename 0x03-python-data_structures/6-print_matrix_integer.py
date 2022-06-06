d880789e023fcc3c579d6b9b7d9387242c3dfe98
Khareemah <ajimobikareemah@gmail.com>
2022-06-06 03:30:32 -0700

    print element in a matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            print("{:d} ".format(matrix[i][j]), end="")
+            if(j == 2):
+                print("{:d}".format(matrix[i][j]), end="")
+            else:
+                print("{:d}".format(matrix[i][j]), end=" ")
         print("")
