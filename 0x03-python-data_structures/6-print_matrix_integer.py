a31b2f58785a87543131975480a4b2b2c781b4eb
Khareemah <ajimobikareemah@gmail.com>
2022-06-06 06:34:59 -0700

    whatever

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        print("None")
     for row in matrix:
         for col in row:
             if col == row[-1]:
