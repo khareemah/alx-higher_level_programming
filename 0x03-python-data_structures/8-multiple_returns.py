547d2d9dfcf981d3e7dd928c64e47df9fe7faa7a
Khareemah <ajimobikareemah@gmail.com>
2022-06-06 04:14:29 -0700

    multiple return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,9 +2,7 @@
 def multiple_returns(sentence):
     my_list = [None]
     my_length = len(sentence)
-    if my_length == 0:
-        my_list[0] = None
-    else:
+    if my_length != 0:
         my_list[0] = my_length
     my_tuple = tuple(my_list)
     return my_length, sentence[0]
