8a583e8c68e7d8e6a50cb19160a0784864ea27b6
Khareemah <ajimobikareemah@gmail.com>
2022-06-06 03:09:53 -0700

    remove Cc

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,7 @@
 #!/bin/bash
 def no_c(my_string):
-    if not my_string:
-        return
     new_string = ""
     for i in my_string:
-        if(i == 'C' or i == 'c'):
-            continue;
-        new_string += i
+        if(i != "C" and i != "c"):
+            new_string += i
     return new_string
