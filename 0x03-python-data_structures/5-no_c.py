e5ee4d1293518b84b2f6e7c9b7c0f690bf7ed0f8
Khareemah <ajimobikareemah@gmail.com>
2022-06-06 06:09:49 -0700

    not c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,7 @@
 #!/bin/bash
 def no_c(my_string):
-    if not my_string:
-        return
     new_string = ""
     for i in my_string:
-        if(i != "C" and i != "c"):
-            new_string += i
+        if(i is not "c" and i is not "C"):
+            new_string = new_string + i
     return new_string
