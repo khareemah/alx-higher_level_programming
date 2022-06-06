c010102310dc25ad7bba3b15b11edffc81acfa6b
Khareemah <ajimobikareemah@gmail.com>
2022-06-06 05:51:20 -0700

    delete at an index

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,7 +2,5 @@
 def delete_at(my_list=[], idx=0):
     if(idx < 0 or idx >= len(my_list)):
         return my_list
-    new_list = my_list.copy()
-    item_to_remove = new_list[idx]
-    del item_to_remove
-    return new_list
+    del my_list[idx]
+    return my_list
