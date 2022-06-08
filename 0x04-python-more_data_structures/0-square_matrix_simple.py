04765df991bb6b75ac33f5c2fb9775e26b6be731
Khareemah <ajimobikareemah@gmail.com>
2022-06-08 04:05:35 -0700

    fix code style

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    result = [[0, 0, 0],[0, 0, 0],[0, 0, 0]]
+    result = [
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0]
+    ]
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
             result[i][j] = matrix[i][j] * matrix[i][j]
