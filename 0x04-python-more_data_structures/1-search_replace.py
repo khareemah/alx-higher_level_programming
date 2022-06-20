19a84ff0c1b763723f4ba08d6e74f54fcb0e6295
Khareemah <ajimobikareemah@gmail.com>
2022-06-20 02:09:42 -0700

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return ([num if num != search else replace for num in my_list])
+    # return ([num if num != search else replace for num in my_list])
+    new_list = my_list.copy()
+    for item in new_list:
+        if item == search:
+            new_list[search] = replace
+    return new_list
