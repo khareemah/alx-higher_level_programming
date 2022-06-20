865a1b7ead9546ae186a3907dbd4b70363ecddf0
Khareemah <ajimobikareemah@gmail.com>
2022-06-20 03:22:04 -0700

    check for non string args

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,9 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    return max(a_dictionary, key=lambda k: a_dictionary[k])
+#    return max(a_dictionary, key=lambda k: a_dictionary[k])
+    max_value = 0
+    for key, value in a_dictionary:
+        if value > max_value:
+            max_value = value
+    return key
