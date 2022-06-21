16d72824e28bc21e01c181697cb3e4a1b64fafc8
Khareemah <ajimobikareemah@gmail.com>
2022-06-21 05:02:56 -0700

    raise exception error

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,21 +1,22 @@
 #!/bin/usr/bin/python3
 
-""" define a class Square """
+"""define a class Square"""
 
 
 class Square:
 
-    """ represent a square """
+    """represent a square"""
     def __init__(self, size=0):
 
-        """ initialize instance data
+        """initialize instance data
             Args:
                 size(int): size of the squarr
         """
+
         if type(size) is int:
             if size < 0:
                 raise ValueError("size must be >= 0")
             else:
-                self._size = size
+                self.__size = size
         else:
             raise TypeError("Size must be an integer")
