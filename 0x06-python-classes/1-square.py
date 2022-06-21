c92517d981b70666141ab3c7cc7184b05cb310a7
Khareemah <ajimobikareemah@gmail.com>
2022-06-21 04:30:19 -0700

    remove white space

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,9 +7,9 @@ class Square:
     """represents a square"""
 
     def __init__(self, size):
-    
+
         """initialize data
             Args:
                 size(str): size of the square
-        """    
+        """
         self.__size = size
