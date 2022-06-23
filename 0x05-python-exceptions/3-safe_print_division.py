57902171d8844c29140c11b6d54f67b6ce50bccb
Khareemah <ajimobikareemah@gmail.com>
2022-06-23 03:15:34 -0700

    task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 
-def safe_print_list(my_list=[], x=0):
-    counter = 0
-    for pr in range(x):
-        try:
-            print("{}".format(my_list[pr]), end='')
-            counter = counter + 1
-        except IndexError:
-            break
-    print("")
-    return counter
+def safe_print_division(a, b):
+    result = 0
+    print("Inside result: ", end='')
+    try:
+        result = a / b
+    except:
+        result = None
+    finally:
+        print("{}".format(result))
+        return result
