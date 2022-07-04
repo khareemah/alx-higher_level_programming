585989c66ca452c18f93987f85a9e814c29bfad2
Khareemah <ajimobikareemah@gmail.com>
2022-07-04 06:24:38 -0700

    task 9, 10, 11

--- 0x0A-python-inheritance/10-sqaure.py
+++ 0x0A-python-inheritance/10-sqaure.py
@@ -32,8 +32,34 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         """Initializes the subclass
         """
+
         self.integer_validator("width", width)
         self.__width = width
 
         self.integer_validator("height", height)
         self.__height = height
+
+    def area(self):
+        """Returns the area for a rectangle
+        """
+
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Returns a defined rectangle description
+        """
+
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
+
+
+class Square(Rectangle):
+    """Class that inherits from Rectangle but makes a square
+    """
+
+    def __init__(self, size):
+        """Initializes a square
+        """
+
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -32,8 +32,40 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         """Initializes the subclass
         """
+
         self.integer_validator("width", width)
         self.__width = width
 
         self.integer_validator("height", height)
         self.__height = height
+
+    def area(self):
+        """Returns the area for a rectangle
+        """
+
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Returns a defined rectangle description
+        """
+
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
+
+
+class Square(Rectangle):
+    """Class that inherits from Rectangle but makes a square
+    """
+
+    def __init__(self, size):
+        """Initializes a square
+        """
+
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """Prints a designated output to define square
+        """
+
+        return "[Square] {}/{}".format(self.__size, self.__size)
